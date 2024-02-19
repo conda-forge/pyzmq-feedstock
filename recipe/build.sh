ae70365781a289451624021e12e0f788d49cfac5
Min RK <benjaminrk@gmail.com>
2024-02-19 12:56:44 +0100

    test pyzmq 26 prelease
    don't need prefix
    
    pkg-config / cmake will find it

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    # We disable zmq version checking which fails under certain compile conditions.
-    # The tests after building are assertion enough that we have built correctly.
-    echo "[global]" > setup.cfg
-    echo "skip_check_zmq = True" >> setup.cfg
-fi
-export ZMQ_PREFIX=$PREFIX
+# make sure it doesn't fallback on bundled libzmq
+export PYZMQ_NO_BUNDLE=1
 
 "${PYTHON}" -m pip install .
