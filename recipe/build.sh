c2cfa14cf222622b86562272890fd455edda0e6d
Min RK <benjaminrk@gmail.com>
2024-02-20 22:30:28 +0100

    checkpoint

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+set -x
 # make sure it doesn't fallback on bundled libzmq
 export PYZMQ_NO_BUNDLE=1
 export SKBUILD_CMAKE_VERBOSE=true
 echo $PYTHON
 which python
 $PYTHON -m sysconfig || true
+echo '----'
+$BUILD_PREFIX/venv/bin/python -m sysconfig || true
 
 $PYTHON -m pip install -vv .
